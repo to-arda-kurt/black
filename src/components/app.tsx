dc7b2710c01c767ac5fc042dc1f33bd38473ad09
Arda Kurt <to.arda.kurt@gmail.com>
2024-07-29 21:52:32 +0100

    Initial

---
+++ src/components/app.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function App() {
+  return (
+    <Flex direction="column" gap="5">
+      <Text>Deploy Repo to Cloudflare</Text>
+      <Button>Let's go</Button>
+    </Flex>
+  );
+}
